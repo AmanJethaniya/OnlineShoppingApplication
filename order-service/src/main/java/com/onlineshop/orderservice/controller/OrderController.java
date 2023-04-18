ffe23f0c3f88ddd5dde831c827ccb74033f7543b
Aman Jethaniya <amanjethaniya619@gmail.com>
2023-04-18 20:00:20 +0530

    Added properties

--- order-service/src/main/java/com/onlineshop/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/onlineshop/orderservice/controller/OrderController.java
@@ -10,6 +10,8 @@ import com.onlineshop.orderservice.model.OrderRequest;
 import com.onlineshop.orderservice.service.OrderService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
@@ -20,13 +22,15 @@ public class OrderController {
 	
 	@PostMapping
 	@CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
+	@TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
 	public String placeOrder(@RequestBody OrderRequest orderRequest) {
 		log.info("Place order triggered, Inside controller");
 		try {
 			service.placeOrder(orderRequest);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			return fallbackMethod(orderRequest, new RuntimeException() );
+			return "facing issue";
+			//return fallbackMethod(orderRequest, new RuntimeException() );
 		}
 		return "Order Placed successfully";
 		
